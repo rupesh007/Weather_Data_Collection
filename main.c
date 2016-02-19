5c110513041dd890263c365e017480d1f4ff7deb
Rupesh <rupesh.basnet007@gmail.com>
2016-02-19 02:18:17 +0200

    Added files via upload

--- main.c
+++ main.c
@@ -4,33 +4,43 @@
 
 #include <m8c.h>
 #include "PSoCAPI.h"
-#include"PSoCGPIOINT.h"
 
-//byte sent_data;
-BYTE data;
+void initialisation();
+
+BYTE data_received;
+
+
+
 void main()
-{ char *key_input;
 
-//int i;
-M8C_EnableGInt;
-UART_Start(UART_PARITY_NONE); //Starts d UART enabeling null parity
-UART_EnableInt();
-UART_IntCntl(UART_ENABLE_RX_INT);
-UART_IntCntl(UART_ENABLE_TX_INT);
-
-while(1)
 {
-//data=UART_bReadRxData(); 
-//data=UART_iReadChar();
-//UART_SendData(data);
-//UART_PutSHexByte(data);
-
-*key_input = UART_cGetChar(); //Reads character from the Rx port
-//if(UART_TX_BUFFER_EMPTY){
-UART_PutChar(*key_input); //Sends a null terminated (RAM)string i.e variable to Tx
-UART_bReadTxStatus();
-UART_PutCRLF();
-//}
+		
+		initialisation();
+		
+	while(1) {
+		
+				if(UART_RX_REG_FULL){//to make sure if rx register has unread data
+					data_received=UART_bReadRxData(); //reads rx register and returns the data					
+					
+						UART_SendData(data_received); //initiates data transmission loading tx buffer
+					    UART_PutSHexByte(data_received);
+						
+						}
+				
+				
+			
 }
-
+}
+void initialisation(){
+       // Counter8_Start();
+		//Counter8_EnableInt;
+		//Counter8_WritePeriod(155); // Set up baud rate generator
+        //Counter8_WriteCompareValue(77);
+		UART_IntCntl(UART_ENABLE_RX_INT);
+		UART_IntCntl(UART_ENABLE_TX_INT);
+	
+	
+		UART_Start(UART_PARITY_NONE); // Enable UART
+		
+		M8C_EnableGInt ; // Turn on interrupts
 }
